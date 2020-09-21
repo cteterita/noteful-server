003809be4d96e26d71e0140f291b57faee8b0319
Claire Lesh <cteterita@gmail.com>
2020-09-21 13:43:53 -0700

    added migrations and seeds, postgrator config

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,10 @@
+INSERT INTO notes (note_name, content, folder_id)
+VALUES
+    ('Cats', 'Cats have claws!', 4),
+    ('Bats', 'Bats have weird feet!', 4),
+    ('Dogs', 'Dogs have big eyes!', 4),
+    ('Important Message', 'Remember to do a thing', 1),
+    ('Important Message 2', 'Remember to do another thing', 1),
+    ('George Bush', 'Call him back', 3),
+    ('Barack Obama', 'Call him back', 3),
+    ('SUPER important thing', 'The most important', 2);
